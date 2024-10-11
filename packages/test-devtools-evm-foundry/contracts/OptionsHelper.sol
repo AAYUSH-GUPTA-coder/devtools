44a1a1368b31fff7ff8f2dfed003e51013e69818
shankar <shankar@layerzerolabs.org>
2024-10-11 16:18:49 +0000

    feat: executor options are parsed for multiple addExecutorLzReceiveOption() calls w/ testing
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/test-devtools-evm-foundry/contracts/OptionsHelper.sol
+++ packages/test-devtools-evm-foundry/contracts/OptionsHelper.sol
@@ -68,14 +68,49 @@ contract OptionsHelper {
     ) internal view returns (bool exist, bytes memory option) {
         (bytes memory executorOpts, ) = ulnOptions.decode(_options);
 
+        // uint128 - 128 bits = 16 bytes = 0x10
+        uint256 U128_LENGTH = 0x10;
         uint256 cursor;
+
+        uint128 lz_gas;
+        uint128 lz_value;
+
         while (cursor < executorOpts.length) {
             (uint8 optionType, bytes memory op, uint256 nextCursor) = this.nextExecutorOption(executorOpts, cursor);
+
+            uint128 newGas;
+            uint128 newValue;
+
+            // author: Shankar - Integrations team
             if (optionType == _executorOptionType) {
-                return (true, op);
+                assembly {
+                    // Grab the gas value from the first 16 bytes
+                    let _gas := mload(add(op, U128_LENGTH))
+
+                    // Grab the value from the next 16 bytes (if there is none this is just 0)
+                    let _value := mload(add(op, mul(U128_LENGTH, 2)))
+
+                    newGas := add(lz_gas, _gas)
+
+                    // To improve readability I am not using lz_value := add(lz_value, _value) directly
+                    newValue := add(lz_value, _value)
+                }
+
+                // The value can not overflow as there is a max limit specified which is 0.1 ether
+                // The gas can overflow and so we have a check for that
+                if (newGas < lz_gas) {
+                    revert("OptionsHelper: gas overflow");
+                }
+
+                lz_gas = newGas;
+                lz_value = newValue;
             }
+
             cursor = nextCursor;
         }
+
+        bytes memory payload = abi.encodePacked(lz_gas, lz_value);
+        return (true, payload);
     }
 
     function nextExecutorOption(
